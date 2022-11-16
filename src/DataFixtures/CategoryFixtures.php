6f0a39101e078f8c7eb92a6c219c58082555052c
Franck <depoorter.franck@gmail.com>
2022-11-16 15:26:00 +0100

    create class fixture and add nex categories

---
+++ src/DataFixtures/CategoryFixtures.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\Category;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class CategoryFixtures extends Fixture
+
+{
+    const CATEGORIES = [
+        "Action",
+        "Animation",
+        "Aventure",
+        "Comédie",
+        "Fantastique",
+        "Horreur",
+        "Guerre",
+        "Documentaire",
+        "Western",
+        "Musical",
+        "Dramatique",
+        "Biographique",
+    ];
+    public function load(ObjectManager $manager)
+    {
+        foreach (self::CATEGORIES as $key => $categoryName) {
+            $category = new Category();
+            $category->setName($categoryName);
+            $manager->persist($category);
+        }
+        $manager->flush();
+    }
+}
